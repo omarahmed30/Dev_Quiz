bd705bb391615f0d34ec05d565bef20e23b4ab9e
Minhal <mag166@case.edu>
2018-05-31 16:54:50 -0400

    radio buttons capture all stuff

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -23,37 +23,49 @@ class QuizzesController < ApplicationController
 
     def validate
         @v = []
-        
-        @answer = Question.find(params[:question1]).answer
-        if @answer == params[:option1]
-            @v.push(Question.find(params[:question1])["option_#{params[:option1]}"] + " is Correct")
-
-        else
-            @v.push(Question.find(params[:question1])["option_#{params[:option1]}"] + " is incorrect")
+        Question.all.size.times do |counter|
+            counter = counter+1
+            @answer = Question.find(params["question#{counter}".to_sym]).answer
+            if @answer == params["option#{counter}".to_sym]
+                @v.push(Question.find(params["question#{counter}".to_sym])["option_#{params["option#{counter}".to_sym]}"] + " is Correct")
+    
+            else
+                @v.push(Question.find(params["question#{counter}".to_sym])["option_#{params["option#{counter}".to_sym]}"] + " is incorrect")
+            end
         end
 
-        @answer = Question.find(params[:question2]).answer
-        if @answer == params[:option2]
-            @v.push(Question.find(params[:question2])["option_#{params[:option2]}"] + " is Correct")
 
-        else
-            @v.push(Question.find(params[:question2])["option_#{params[:option2]}"] + " is incorrect")
-        end
 
-        @answer = Question.find(params[:question3]).answer
-        if @answer == params[:option3]
-            @v.push(Question.find(params[:question3])["option_#{params[:option3]}"] + " is Correct")
+    #     @answer = Question.find(params[:question1]).answer
+    #     if @answer == params[:option1]
+    #         @v.push(Question.find(params[:question1])["option_#{params[:option1]}"] + " is Correct")
 
-        else
-            @v.push(Question.find(params[:question3])["option_#{params[:option3]}"] + " is incorrect")
-        end
+    #     else
+    #         @v.push(Question.find(params[:question1])["option_#{params[:option1]}"] + " is incorrect")
+    #     end
 
-        @answer = Question.find(params[:question4]).answer
-        if @answer == params[:option4]
-            @v.push(Question.find(params[:question4])["option_#{params[:option4]}"] + " is Correct")
+    #     @answer = Question.find(params[:question2]).answer
+    #     if @answer == params[:option2]
+    #         @v.push(Question.find(params[:question2])["option_#{params[:option2]}"] + " is Correct")
 
-        else
-            @v.push(Question.find(params[:question4])["option_#{params[:option4]}"] + " is incorrect")
-        end
+    #     else
+    #         @v.push(Question.find(params[:question2])["option_#{params[:option2]}"] + " is incorrect")
+    #     end
+
+    #     @answer = Question.find(params[:question3]).answer
+    #     if @answer == params[:option3]
+    #         @v.push(Question.find(params[:question3])["option_#{params[:option3]}"] + " is Correct")
+
+    #     else
+    #         @v.push(Question.find(params[:question3])["option_#{params[:option3]}"] + " is incorrect")
+    #     end
+
+    #     @answer = Question.find(params[:question4]).answer
+    #     if @answer == params[:option4]
+    #         @v.push(Question.find(params[:question4])["option_#{params[:option4]}"] + " is Correct")
+
+    #     else
+    #         @v.push(Question.find(params[:question4])["option_#{params[:option4]}"] + " is incorrect")
+    #     end
     end
 end
