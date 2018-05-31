d04312703a4a775e86abecf531297e0c774f8154
Minhal <mag166@case.edu>
2018-05-31 16:08:07 -0400

    shows results for all questions

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -22,15 +22,38 @@ class QuizzesController < ApplicationController
 
 
     def validate
-        @answer = Question.find(params[:question]).answer
-        if @answer == params[:option]
-            @v = params[:option] + " is Correct"
+        @v = []
+        
+        @answer = Question.find(params[:question1]).answer
+        if @answer == params[:option1]
+            @v.push(Question.find(params[:question1])["option_#{params[:option1]}"] + " is Correct")
+
         else
-            @v = params[:option] + " is incorrect"
+            @v.push(Question.find(params[:question1])["option_#{params[:option1]}"] + " is incorrect")
         end
 
+        @answer = Question.find(params[:question2]).answer
+        if @answer == params[:option2]
+            @v.push(Question.find(params[:question2])["option_#{params[:option2]}"] + " is Correct")
 
+        else
+            @v.push(Question.find(params[:question2])["option_#{params[:option2]}"] + " is incorrect")
+        end
 
-        
+        @answer = Question.find(params[:question3]).answer
+        if @answer == params[:option3]
+            @v.push(Question.find(params[:question3])["option_#{params[:option3]}"] + " is Correct")
+
+        else
+            @v.push(Question.find(params[:question3])["option_#{params[:option3]}"] + " is incorrect")
+        end
+
+        @answer = Question.find(params[:question4]).answer
+        if @answer == params[:option4]
+            @v.push(Question.find(params[:question4])["option_#{params[:option4]}"] + " is Correct")
+
+        else
+            @v.push(Question.find(params[:question4])["option_#{params[:option4]}"] + " is incorrect")
+        end
     end
 end
