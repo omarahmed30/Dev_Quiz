8ebace1536eea27531119617a0c05d66edf823e7
CatPerry <27353719+CatPerry@users.noreply.github.com>
2018-06-04 12:31:15 -0400

    styling

--- app/controllers/quiz_questions_controller.rb
+++ app/controllers/quiz_questions_controller.rb
@@ -1,40 +1,29 @@
 class QuizQuestionsController < ApplicationController
-
-
 	def index
-
 		$questions = Question.where(subject:params[:subject], difficulty:params[:difficulty])
-
-		# @question = Question.find_by(params[:id])
-		# if @question.answer == params[:option]
-		# 		@v = params[:option]
-		# end
 	end
 
 	def show
 		$questions = Question.all	
-    end
+	end
 
-    def new
-    end
+	def new
+	end
 
-    def edit
-    end
+	def edit
+	end
 
-    def create
-    end
+	def create
+	end
 
-    def update
-    end
+	def update
+	end
 
-    def destroy
+	def destroy
 	end
 	
-
-
 	def validate
 		# puts params
-
 		@result = []
 		@questions = []
 
@@ -44,21 +33,12 @@ class QuizQuestionsController < ApplicationController
 			@answer = Question.find(params["question#{id}".to_sym]).answer
 			if @answer == params["q#{id}".to_sym]
 				@questions.push(Question.find(params["question#{id}".to_sym]).description)
-				@result.push(Question.find(params["question#{id}".to_sym])["option_#{params["q#{id}".to_sym]}"] + " is Correct")
+				@result.push(Question.find(params["question#{id}".to_sym])["option_#{params["q#{id}".to_sym]}"] + " is CORRECT")
 	
 			else
 				@questions.push(Question.find(params["question#{id}".to_sym]).description)
-				@result.push(Question.find(params["question#{id}".to_sym])["option_#{params["q#{id}".to_sym]}"] + " is incorrect")
+				@result.push(Question.find(params["question#{id}".to_sym])["option_#{params["q#{id}".to_sym]}"] + " is INCORRECT")
 			end
 		end
 	end
 end
-
-
-
-
-
-
-
-
-
--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,14 +1,7 @@
 class QuizzesController < ApplicationController
-  
-  
-  def validate
-    
-    @v = params[:option]
-
-
-
-   end
-
+    def validate  
+        @v = params[:option]
+    end
 
     def index
     end
@@ -31,7 +24,6 @@ class QuizzesController < ApplicationController
     def destroy
     end
 
-
     def validate
         @v = []
         Question.all.size.times do |counter|
@@ -44,64 +36,5 @@ class QuizzesController < ApplicationController
                 @v.push(Question.find(params["question#{counter}".to_sym])["option_#{params["option#{counter}".to_sym]}"] + " is incorrect")
             end
         end
-
-
-
-    #     @answer = Question.find(params[:question1]).answer
-    #     if @answer == params[:option1]
-    #         @v.push(Question.find(params[:question1])["option_#{params[:option1]}"] + " is Correct")
-
-    #     else
-    #         @v.push(Question.find(params[:question1])["option_#{params[:option1]}"] + " is incorrect")
-    #     end
-
-    #     @answer = Question.find(params[:question2]).answer
-    #     if @answer == params[:option2]
-    #         @v.push(Question.find(params[:question2])["option_#{params[:option2]}"] + " is Correct")
-
-    #     else
-    #         @v.push(Question.find(params[:question2])["option_#{params[:option2]}"] + " is incorrect")
-    #     end
-
-    #     @answer = Question.find(params[:question3]).answer
-    #     if @answer == params[:option3]
-    #         @v.push(Question.find(params[:question3])["option_#{params[:option3]}"] + " is Correct")
-
-    #     else
-    #         @v.push(Question.find(params[:question3])["option_#{params[:option3]}"] + " is incorrect")
-    #     end
-
-    #     @answer = Question.find(params[:question4]).answer
-    #     if @answer == params[:option4]
-    #         @v.push(Question.find(params[:question4])["option_#{params[:option4]}"] + " is Correct")
-
-    #     else
-    #         @v.push(Question.find(params[:question4])["option_#{params[:option4]}"] + " is incorrect")
-    #     end
     end
-
-
-    def html 
-
-    end
-
-
-    def css 
-
-    end
-
-
-    def ruby
-
-    end
-
-
-    def javascript
-
-    end
-
-    def cards 
-
-    end
-
 end
